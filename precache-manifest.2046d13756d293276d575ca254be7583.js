b3fdf79c4eca2c49dc507e349ce9f38808c564d6
Jimi Loc Nguyen <jimilocn@gmail.com>
2019-09-08 11:45:08 -0500

    Updates

--- precache-manifest.2046d13756d293276d575ca254be7583.js
+++ precache-manifest.2046d13756d293276d575ca254be7583.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "981f5fc9ed33a6fb33dceb5e664dcd63",
+    "revision": "9b3010268804c800c1b7b05717683eca",
     "url": "/Garden-Clicky-Game-Live/index.html"
   },
   {
-    "revision": "803268fe741cf28fa8b1",
-    "url": "/Garden-Clicky-Game-Live/static/css/main.d3a5d28e.chunk.css"
+    "revision": "bbaa867e9a843ce55530",
+    "url": "/Garden-Clicky-Game-Live/static/css/main.cfddfda8.chunk.css"
   },
   {
     "revision": "606b67ae68288e2d616e",
     "url": "/Garden-Clicky-Game-Live/static/js/2.5f124323.chunk.js"
   },
   {
-    "revision": "803268fe741cf28fa8b1",
-    "url": "/Garden-Clicky-Game-Live/static/js/main.b5cd6492.chunk.js"
+    "revision": "bbaa867e9a843ce55530",
+    "url": "/Garden-Clicky-Game-Live/static/js/main.3d944690.chunk.js"
   },
   {
     "revision": "6782fd8ce3f2bf04537d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Garden-Clicky-Game-Live/precache-manifest.131c7755b5b1cbd091ebf70f6e4ecf31.js"
+  "/Garden-Clicky-Game-Live/precache-manifest.2046d13756d293276d575ca254be7583.js"
 );
 
 self.addEventListener('message', (event) => {
